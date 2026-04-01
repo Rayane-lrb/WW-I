32569bcba6c4f62faf4cb6fb13d5784405729be0
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-04-02 00:27:21 +0200

    feat: filters ready to use

--- src/main.js
+++ src/main.js
@@ -1,52 +1,53 @@
-  "use strict";
-  const body = document.body;
-  const themeBtn = document.getElementById("themeBtn");
-  const themeBtnImg = document.getElementById("themeBtn-img");
-  const resultContainer = document.getElementById("results-container");
-  const searchBar = document.getElementById("searchInput");
-  
-  function themeSwitch(theme) {
-    body.classList.remove("light-theme", "dark-theme");
-    body.classList.add(theme);
-    
-    if(theme === "light-theme") {
+"use strict";
+const body = document.body;
+const themeBtn = document.getElementById("themeBtn");
+const themeBtnImg = document.getElementById("themeBtn-img");
+const resultContainer = document.getElementById("results-container");
+const searchBar = document.getElementById("searchInput");
+const sortSelect = document.getElementById("sortSelect");
+
+function themeSwitch(theme) {
+  body.classList.remove("light-theme", "dark-theme");
+  body.classList.add(theme);
+
+  if (theme === "light-theme") {
     themeBtnImg.src = "/public/moon.svg";
-    } else if(theme === "dark-theme")
-      {
-      themeBtnImg.src = "/public/sun.svg";
-    }
+  } else if (theme === "dark-theme") {
+    themeBtnImg.src = "/public/sun.svg";
   }
+}
 
-  themeBtn.addEventListener("click", () => {
-    const newTheme = body.classList.contains("dark-theme")
-      ? "light-theme"
-      : "dark-theme";
-    localStorage.setItem("theme", newTheme);
-    themeSwitch(newTheme);
-  });
+themeBtn.addEventListener("click", () => {
+  const newTheme = body.classList.contains("dark-theme")
+    ? "light-theme"
+    : "dark-theme";
+  localStorage.setItem("theme", newTheme);
+  themeSwitch(newTheme);
+});
 
-  const savedTheme = localStorage.getItem("theme") || "light-theme";
-  window.addEventListener("load", () => {
-    themeSwitch(savedTheme);
-    fetchImages();
-  });
+const savedTheme = localStorage.getItem("theme") || "light-theme";
+window.addEventListener("load", () => {
+  themeSwitch(savedTheme);
+  fetchImages();
+});
 
-  async function fetchImages() {
-    try {
-    const response = await fetch("https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/parcours_street_art/records?limit=20");
+async function fetchImages() {
+  try {
+    const response = await fetch(
+      "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/parcours_street_art/records?limit=20",
+    );
     const data = await response.json();
 
     displayImages(data);
-    }
-    catch(error) {
-      throw new Error(error);
-    }
+  } catch (error) {
+    throw new Error(error);
   }
+}
 
-  function displayImages(data) {
-    data.results.forEach((item) => {
-        const divCard = document.createElement("div");
-        divCard.classList.add("card", "hidden");
+function displayImages(data) {
+  data.results.forEach((item) => {
+    const divCard = document.createElement("div");
+    divCard.classList.add("card", "hidden");
     divCard.innerHTML = `
     <img src="${item.url_image ? item.url_image.url : "/placeholder.jpg"}" loading="lazy"></img>
     <div class="card-info">
@@ -61,16 +62,50 @@
     `;
     resultContainer.appendChild(divCard);
     observer.observe(divCard);
-    })
-  }
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if(entry.isIntersecting) {
-        entry.target.classList.remove("hidden");
-        entry.target.classList.add("visible");
-        observer.unobserve(entry.target);
-      }
-    });
   });
+}
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.remove("hidden");
+      entry.target.classList.add("visible");
+      observer.unobserve(entry.target);
+    }
+  });
+});
 
+sortSelect.addEventListener("change", () => {
+  const selectedValue = sortSelect.value;
+  sortCards(selectedValue);
+})
+
+function sortCards(selectedValue) {
+  const cards = Array.from(document.querySelectorAll(".card"));
+
+  cards.sort((a,b) => {
+    const nameA = a.querySelector("h3").textContent;
+    const nameB = b.querySelector("h3").textContent;
+    const dateA = new Date(a.querySelector(".name_date_div h5").textContent);
+    const dateB = new Date(b.querySelector(".name_date_div h5").textContent);        
+
+    if(selectedValue === "az") return nameA.localeCompare(nameB);
+    if(selectedValue === "za") return nameB.localeCompare(nameA);
+    if(selectedValue === "nieuwst") return dateB - dateA;
+    if(selectedValue === "oudste") return dateA - dateB;
+  });
+  cards.forEach((card) => resultContainer.appendChild(card));
+}
+
+searchBar.addEventListener("input", () => {
+  const searchedValue = searchBar.value;
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    if(card.textContent.toLocaleLowerCase().includes(searchedValue.toLowerCase())) {
+      card.style.display = "flex";
+    } else {
+      card.style.display = "none";
+    }
+  })
+})
 
