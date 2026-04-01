9cf0387d9cb99d087627783dff7b02782cb06ab5
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-04-01 11:40:11 +0200

    feat: theme switch function

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const body = document.body;
+const themeBtn = document.getElementById("themeBtn");
+
+function themeSwitch(theme) {
+  body.classList.remove("light-theme, dark-theme");
+  body.classList.add(theme);
+}
+
+themeBtn.addEventListener("click", () => {
+  const newTheme = body.classList.contains("dark-theme") ? "light-theme" : "dark-theme";
+  localStorage.setItem("theme", newTheme);
+  themeSwitch(newTheme);
+});
